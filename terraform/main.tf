972913344bc8f75f2645d8ce7d21a7cacd399aba
MarkTechOff <mark.conway@microfocus.com>
2022-10-31 13:54:47 +0000

    See if account quota increased

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,7 +17,7 @@ module "sandbox" {
 
   change_management_parameters = {
     change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+    change_reason       = "See if account quota has increased"
   }
 
   custom_fields = {
