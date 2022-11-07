619ed396493a7ca3fd2e0e5e3467534d3cfa3a73
MarkTechOff <mark.conway@microfocus.com>
2022-11-07 13:05:27 +0000

    Add production account

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,3 +26,37 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+
+
+
+# Creating a production account
+module "sandbox" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "mark.conway+prod1@microfocus.com"
+    AccountName               = "prod1"
+    ManagedOrganizationalUnit = "nglz"
+    SSOUserEmail              = "mark.conway+prod1@microfocus.com"
+    SSOUserFirstName          = "Production"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+    "Owner" = "Mark Conway"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Create a production account"
+  }
+
+  custom_fields = {
+    group = "prod"
+  }
+
+  account_customizations_name = "production"
+}
+
